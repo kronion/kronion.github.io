9eb7a0181e8b62836c009f8cd139898837da293d
Collin Stedman <cstedman@princeton>
2014-02-23 16:18:48 -0500

    HackPrinceton Testing

---
+++ test/js/script.js
@@ -0,0 +1,60 @@
+$(".signup").click(function () {
+    $('html, body').animate({
+        scrollTop: $('#signup').offset().top
+    }, 'slow');
+});
+
+$(".schedule").click(function () {
+    $('html, body').animate({
+        scrollTop: $('#schedule').offset().top
+    }, 'slow');
+});
+
+$(".hire").click(function () {
+    $('html, body').animate({
+        scrollTop: $('#hire').offset().top
+    }, 'slow');
+});
+
+$(".faq").click(function () {
+    $('html, body').animate({
+        scrollTop: $('#faq').offset().top
+    }, 'slow');
+});
+
+$(".sponsor").click(function () {
+    $('html, body').animate({
+        scrollTop: $('#sponsor').offset().top
+    }, 'slow');
+});
+
+$(document).ready(function () {
+    $('.header').css({
+        'position': 'relative',
+        'background': 'url(img/bg/bg' + Math.floor(Math.random() * 8) + '.jpg) no-repeat center center fixed',
+        'background-position': 'center',
+        'background-size': 'cover',
+        '-webkit-background-size': 'cover',
+        '-moz-background-size': 'cover',
+        '-o-background-size': 'cover',
+        'text-align': 'center',
+        'height': '100vh',
+        'min-height': '600px',
+        'max-height': '1500px'
+    });
+  
+  $('#student').change(function () {
+    if ($(this).val() == "princeton") {
+      $('#prince').slideDown('fast');
+      $('#other').slideUp('fast');
+    } else if ($(this).val() == "school") {
+      $('#other').slideDown('fast');
+      $('#prince').slideUp('fast');
+    } 
+    else {
+      $('#other').slideUp('fast');
+      $('#prince').slideUp('fast');
+
+    }
+  });
+});
